6bfd070982136ec5dc9f65f3b1a3307e1065abaa
Torey Hickman <torey@toreyhickman.com>
2013-08-06 23:50:30 -0500

    give model responsibility of formatting the display of the names

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,8 @@ class Student < ActiveRecord::Base
   validates_presence_of :last_name
 
   scope :currently_enrolled, -> { where(enrolled: true) }
+
+  def display_last_name_first_name
+    "#{last_name}, #{first_name}"
+  end
 end
